4c3009e7001872b65e1cd5e34b318421156f37aa
Azizbek Abdukhakimov <azizbekabdukhakimov007@gmail.com>
2025-08-02 17:05:50 +0500

    fix: Conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom"; // ✅ to'g'ri import
+import { BrowserRouter } from "react-router-dom"; 
 import MainRoutes from "./routes/MainRoutes";
 
 function App() {
   return (
     <BrowserRouter>
-      <MainRoutes /> {/* ✅ route larni chaqirish */}
+      <MainRoutes /> 
     </BrowserRouter>
   );
 }
